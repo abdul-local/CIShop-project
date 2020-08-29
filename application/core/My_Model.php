17be50de266ff75a358ae313d2f034211cea3ba9
abdul-local <abdullah.hamzan@gmail.com>
2020-08-29 12:37:23 +0800

    Membuat Fungsi Umum dari Query Database

--- application/core/My_Model.php
+++ application/core/My_Model.php
@@ -1,8 +1,30 @@
 <?php
 defined('BASEPATH') OR exit('No direct access allowed');
 class My_Model extends CI_Model{
-    
-}
+    protected $table = ' ';
+    // buat fungsi untuk construct
+    public function _construct(){
+        parent::_construct();
+        // kita cek tabel
+        if(!$this->table){
+            $this->table = strtolower (
+                str_replace('_model','',get_class($this))
+            );
+        }
+       
 
+    }
+     // buat method validation input
+     public function validate(){
+        $this->load->library('form_validation');
+        $this->form_validation->set_error_delimeters(
+            '<small class="form-text text-danger">','</small>'
+
+        );
+        $validateRule=$this->getValidationRules();
+        $this->form_validation->set_rules($validateRule);
+        return $this->form_validation->run();
+}
+}
 
 ?>
