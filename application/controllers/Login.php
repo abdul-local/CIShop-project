e52112877964cd3cf940445cb203b2f89f21667e
abdul-local <abdullah.hamzan@gmail.com>
2020-09-01 13:18:28 +0800

    Membuat Controller untuk Login

---
+++ application/controllers/Login.php
@@ -0,0 +1,42 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Login extends MY_Controller {
+    public function __construct(){
+        parent:: __construct();
+        $is_login = $this->sesssion->userdata('is_login');
+        if($is_login){
+            redirect(base_url());
+            return;
+        }
+    }
+    public function index(){
+        if(!$_POST){
+            $input = (object) $this->login->getDefalutValues();
+        }else{
+            $input = (object) $this->input->post(null,true);
+        }
+        if(!$this->login->validate()){
+            $data['title'] ='login';
+            $data['input'] =$input;
+            $data['page'] ='auth/login';
+
+            $this->view($data);
+            return ;
+
+        }
+        if($this->login->run($input)){
+            $this->session->set_flashdata('success','Berhasil Login');
+            redirect(base_url());
+        }else{
+            $this->session->set_flashdata('error','E-mail atau Password sedang tidak aktive');
+            redirect(base_url('login'));
+        }
+    }
+
+
+}
+
+
+
+
+?>
