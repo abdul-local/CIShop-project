a26fd3e27195ff6af30ae9d8666b100ab0c25346
abdul-local <abdullah.hamzan@gmail.com>
2020-09-02 17:44:23 +0800

    Membuat Tabel dan Model Produck

---
+++ application/models/Product_model.php
@@ -0,0 +1,64 @@
+<?php
+defined('BASEPATH') OR exit('No script allowed access');
+
+class Product_model extends MY_Model {
+    protected $perPage = 5;
+
+    //buat method defaultValues
+    public function getDefaultValues(){
+        return [
+            'id_category'=>'',
+            'slug' =>'',
+            'title'=>'',
+            'description'=>'',
+            'price'=>'',
+            'is_avaible'=>1,
+            'image'=>'',
+
+        ];
+
+
+    }
+    //buat metod untuk validationrules
+    public function getValidationRules(){
+        $validationRules=[
+            [
+                'field'=>'id_category',
+                'label'=>'Kategori',
+                'rules'=>'required',
+            ],
+            [
+                'field'=>'slug',
+                'label'=>'Slug',
+                'rules'=>'trim|required|callback_unique_slug',
+            ],
+
+            [
+                'field'=>'title',
+                'label'=>'Nama Produk',
+                'rules'=>'trim|required',
+            ],
+            [
+                'field'=>'descriptio',
+                'label'=>'Deskripsi',
+                'rules'=>'trim|required',
+            ],
+            [
+                'field'=>'price',
+                'label'=>'Harga',
+                'rules'=>'trim|required|numeric',
+            ],
+            [
+                'field'=>'is_avaible',
+                'label'=>'Ketersediaan',
+                'rules'=>'trim|required',
+            ],
+
+            ];
+            
+    }
+
+    
+}
+
+?>
