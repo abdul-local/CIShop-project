11c644d0e92ef896375420c63c54d7da8e6b4f0a
abdul-local <abdullah.hamzan@gmail.com>
2020-09-05 21:20:37 +0800

    Implentasi Formulir Tambah Product dalam Keranjang

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -3,13 +3,13 @@ defined('BASEPATH') or exit ('No script Allowed acces');
 
 // buat class
 class Cart extends MY_Controller {
-    protected $id;
+    private $id;
     // buat method
     public function __construct(){
         parent:: __construct();
         $is_login=$this->session->userdata('is_login');
         $this->id = $this->session->userdata('id');
-        if(!is_login){
+        if(!$is_login){
             redirect(base_url());
             return;
         }
@@ -17,21 +17,21 @@ class Cart extends MY_Controller {
     }
     // buat method add
     public function add(){
-        if(!$_POST){
+        if(!$_POST || $this->input->post('qty') < 1){
+            $this->session->set_flashdata('error','Kuantitas tidak boleh kosong');
             redirect(base_url());
             return;
         }else{
             $input = (object) $this->input->post(null, true);
             $this->cart->table ='product';
             $product = $this->cart->where('id',$input->id_product)->first();
-            $subtotal= $cart->subtotal + $subtotal;
-
-            $cart=$this->cart->where('id_user',$this->id)->where('id_product',$input->id)->first();
             $subtotal =$product->price * $input->qty;
+            $this->cart->table ='cart';
+            $cart= $this->cart->where('id_user',$this->id)->where('id_product',$input->id_product)->first();
             if($cart){
                 $data =[
                     'qty'=>$cart->qty + $input->qty,
-                    '$subtotal'=>$cart->subtotal + $subtotal,
+                    'subtotal'=>$cart->subtotal + $subtotal,
                 ];
                 if($this->cart->where('id',$cart->id)->update($data)){
                     $this->session->set_flashdata('success','Product berhasil di tambah');
@@ -43,9 +43,9 @@ class Cart extends MY_Controller {
 
             $data =[
                 'id_user'=>$this->id,
-                'id_product'-$input->id_product,
-                'qty'=>$cart->$iput->qty,
-                '$subtotal'=>$subtotal,
+                'id_product'=>$input->id_product,
+                'qty'=>$input->qty,
+                'subtotal'=>$subtotal,
             ];
             if($this->cart->create($data)){
                 $this->session->set_flashdata('success','Product berhasil di tambah');
