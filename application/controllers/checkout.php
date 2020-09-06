0b61abf02e7424b23596df28738b2d71b895d388
abdul-local <abdullah.hamzan@gmail.com>
2020-09-06 21:16:52 +0800

    Membuat Fungsi Tambah Order

--- application/controllers/checkout.php
+++ application/controllers/checkout.php
@@ -17,7 +17,7 @@ class Checkout extends MY_Controller{
         }
     }
     // buat method untuk menampikan data
-    public function index(){
+    public function index($input = null){
         $this->checkout->table ='cart';
 
         $data['cart']=$this->checkout->select(
@@ -35,11 +35,53 @@ class Checkout extends MY_Controller{
             redirect(base_url());
         }
 
-
+    $data['input'] = $input ? $input : (object) $this->input->getDefaultValidation();
     $data['title'] ='Checkout';
     $data['page'] ='pages/checkout/index';
     return $this->view($data);
   
+    }
+    // buat method create
+    public function create(){
+        if(!$_POST){
+            redirect(base_url());
+        }else{
+           $input = (object) $this->input->post(null,true);
+        }
+        if(!$this->checkout->validate()){
+            return $this->index($input);
+        }
+        $total= $this->db->select_sum('subtotal')->where('id_user',$this->id)->get('cart')->row()->subtotal;
+        $data =[
+            'id_user'=>$this->id,
+            'date'=>date('Y-m-d'),
+            'invoice'=>$this->id.date('YmdHis'),
+            'total'=>$total,
+            'name'=>$input->name,
+            'adress'=>$input->adress,
+            'phone'=>$input->phone,
+            'status'=>'waiting',
+            
+        ];
+        if($order=$this->checkout->create()){
+            $cart= $this->db->where('id_user',$this->id)
+            ->get('cart')->result_array();
+            foreach($cart as $row){
+                $row['id_orders'] = $order;
+                unset($row['id'],$row['id_user']);
+                $this->db->insert('orders_detail',$row);
+            }
+            $this->db->delete('cart',['id_user'=>$this->id]);
+            $this->session->set_flashdata('success','Data berhasil disimpan ');
+            $data['title']='Checkout success';
+            $data['content']= (object) $data;
+            $data['page']='pages/checkout/success';
+
+            return $this->view($data);
+        }else{
+            $this->session->set_flashdata('error','Opps! terjadi suatu kesalahan');
+            return $this->index($input);
+        }
     }
 
 }
