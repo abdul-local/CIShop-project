9434d3fc262ddcbaead2f77a6c26f636b0fdea0b
abdul-local <abdullah.hamzan@gmail.com>
2020-09-06 18:00:14 +0800

    Membuat Tabel, Model dan Controller Checkout

---
+++ application/controllers/checkout.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('BASEPATH') or exit('No script alloed access');
+//buat classs
+class Checkout extends MY_Controller{
+
+    
+}
+
+
+?>
---
+++ application/models/Checkout_model.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') or exit ('No script access Allowed');
+// buat class
+class Checkout_model extends MY_Model {
+    public $table ='orders';
+    // buat method untuk nilai defaul
+    public function geDefaultValues(){
+        return [
+            'name'=>'',
+            'address'=>'',
+            'phone'=>'',
+            'status'=>'',
+        ];
+
+    }
+    // buat method untuk validationRules
+    public function ValidationRules(){
+        $validationRules =[
+            [
+                'field'=>'name',
+                'label'=>'Nama',
+                'rules'=>'trim|required',
+            ],
+            [
+                'field'=>'address',
+                'label'=>'Alamat',
+                'rules'=>'trim|required',
+            ],
+            [
+                'field'=>'phone',
+                'label'=>'Telepon',
+                'rules'=>'trim|required|max_length[15]',
+            ],
+            ];
+        return $validationRules;
+    }
+
+}
+
+
+
+
+?>
